7f6b0874222aaf71f6db4913545367458c283393
tochasantos <36169633+tochasantos@users.noreply.github.com>
2018-04-29 01:51:29 +0100

    Nome do ficheiro/classe diferente

--- src/users/Director.java
+++ src/users/Director.java
@@ -1,5 +1,10 @@
 package users;
 
-public interface Director extends User{
+/**
+ * A director that collaborates in an audiovisual Production.
+ * @author Antonio Santos 49055 / Raquel Pena 45081.
+ *
+ */
+public interface Director extends User {
 
 }
