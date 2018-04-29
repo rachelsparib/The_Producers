20e968df173b8aed81292a529737ba4abf75ffef
tochasantos <36169633+tochasantos@users.noreply.github.com>
2018-04-29 01:47:05 +0100

    Nome de ficheiro/classe diferente

---
+++ src/users/Actor.java
@@ -0,0 +1,10 @@
+package users;
+
+/**
+ * An actor that collaborates in an audiovisual Production.
+ * @author Antonio Santos 49055 / Raquel Pena 45081.
+ *
+ */
+public interface Actor extends User {
+	
+}
