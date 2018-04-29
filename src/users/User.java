2efe78760a472774051bf2135551201a061a2126
tochasantos <36169633+tochasantos@users.noreply.github.com>
2018-04-29 02:04:59 +0100

    Add files via upload

--- src/users/User.java
+++ src/users/User.java
@@ -1,5 +1,27 @@
 package users;
 
+/**
+ * A collaborator of a audiovisual Production.
+ * @author Antonio Santos 49055 / Raquel Pena 45081.
+ *
+ */
 public interface User {
-
+	
+	/**
+	 * Returns collaborator's name.
+	 * @return name of the collaborator.
+	 */
+	String getName();
+	
+	/**
+	 * Collaborator's cost charged per hour.
+	 * @return cost charged per hour by the collaborator.
+	 */
+	int getHourlyCost();
+	
+	/**
+	 * Returns collaborator's information.
+	 * @return information about the collaborator.
+	 */
+	String toString();
 }
