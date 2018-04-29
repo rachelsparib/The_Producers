b16a7e98795d44a9ad04aa33fc68e9d430b49d10
tochasantos <36169633+tochasantos@users.noreply.github.com>
2018-04-29 02:04:20 +0100

    Mudança de nome da classe

--- src/users/UserClass.java
+++ src/users/UserClass.java
@@ -1,5 +1,48 @@
 package users;
+import util.*;
 
+import java.time.LocalDateTime;
+
+/**
+ * Common behaviors of the different collaborators.
+ * @author Antonio Santos 49055 / Raquel Pena 45081.
+ *
+ */
 public abstract class UserClass implements User {
+	/**
+	 * Stores collaborator's name.
+	 */
+	private String username;
+	
+	/**
+	 * Collaborator's cost charged per hour.
+	 */
+	private int hourlyCost;
+	
+	/**
+	 * Collaborator's calendar with scheduled recordings.
+	 */
+	private Array<LocalDateTime> calendar;
+	
+	/**
+	 * Creates a collaborator in the Production management system.
+	 * @param hourlyCost cost per hour charged by the collaborator.
+	 * @param username collaborator's name.
+	 */
+	protected UserClass(int hourlyCost, String username) {
+		this.username = username;
+		this.hourlyCost = hourlyCost;
+		calendar = new ArrayClass<LocalDateTime>();	//with default size of 50
+	}
+	
 
+	public String getName() {
+		return username;
+	}
+	
+	public int getHourlyCost() {
+		return hourlyCost;
+	}
+	
+	public abstract String toString();
 }
