dfb70adab6ab16728f7e334d53fb100f0275b9a4
tochasantos <36169633+tochasantos@users.noreply.github.com>
2018-04-29 01:47:47 +0100

    Nome de ficheiro/classe diferente + implementacoes

---
+++ src/users/ActorClass.java
@@ -0,0 +1,26 @@
+package users;
+
+import enums.NotorietyTypeEnum;
+import enums.UserTypeEnum;
+
+/**
+ * An implementation of an actor.
+ * @author Antonio Santos 49055 / Raquel Pena 45081.
+ *
+ */
+public class ActorClass extends AbstractUserClass implements Actor {
+	
+	/**
+	 * Creates an actor.
+	 * @param hourlyCost actor's cost charged per hour.
+	 * @param username name of the actor.
+	 */
+	public ActorClass(int hourlyCost, String username) {
+		super(hourlyCost, username);
+	}
+
+	@Override
+	public String toString() {
+		return UserTypeEnum.ACTOR.getName() + " " + NotorietyTypeEnum.NORMAL.getName() + getName() + " " + getHourlyCost();
+	}
+}
