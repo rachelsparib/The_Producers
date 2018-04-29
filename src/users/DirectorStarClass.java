4d990019df137b68a568d2f6726660b8dbb621aa
tochasantos <36169633+tochasantos@users.noreply.github.com>
2018-04-29 02:07:09 +0100

    Add files via upload

--- src/users/DirectorStarClass.java
+++ src/users/DirectorStarClass.java
@@ -1,5 +1,24 @@
 package users;
 
-public class DirectorStarClass extends StarClass implements Director {
+import enums.NotorietyTypeEnum;
+import enums.UserTypeEnum;
 
+/**
+ * An implementation of a director with star notoriety.
+ * @author Antonio Santos 49055 / Raquel Pena 45081.
+ *
+ */
+public class DirectorStarClass extends DirectorClass implements Director {
+	/**
+	 * Creates a director with the notoriety of a star.
+	 * @param hourlyCost actor's cost charged per hour.
+	 * @param username name of the actor.
+	 */
+	public DirectorStarClass(int hourlyCost, String username) {
+		super(hourlyCost, username);
+	}
+	
+	public String toString() {	//method redefinition from DirectorClass
+		return UserTypeEnum.DIRECTOR.getName() + " " +  NotorietyTypeEnum.STAR.getName() + getName() + " " + getHourlyCost();
+	}
 }
